1a8be8251fa016c7d2dfd5c6b152a98de95243ec
rahulk2 <Rahul.Kumar4@amdocs.com>
2025-01-05 10:54:43 +0530

    configured docker compose and check for db up

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+'''
+Calculate functions
+'''
+
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,25 @@
+"""
+Sample test
+"""
+
+from django.test import SimpleTestCase
+
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    """ Test the calc module """
+
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        res = calc.add(5, 6)
+        
+        self.assertEqual(res, 11)
+
+    def test_sub_numbers(self):
+        """Test subtracting numbers together"""
+        res = calc.subtract(10, 5)
+
+        self.assertEqual(res, 5)
+
+
